8343e4372b0b3c74760811fc70f16e93b7029b24
ElisePatrikainen <elise.patrikainen@gmail.com>
2021-03-21 16:36:58 +0100

    03_08

--- src/app/state/selected-items/selected-items.reducer.ts
+++ src/app/state/selected-items/selected-items.reducer.ts
@@ -1,22 +1,26 @@
 import {createReducer, on} from '@ngrx/store';
 import {itemSelected} from './selected-items.actions';
 
-// interface !!
+/* L'état devenant plus complexe, l'ajout d'une interface ou d'une classe typant l'état est recommandé */
+interface State {
+  ids: number[];
+  q: number;
+}
 
-const initialState = {
+const initialState: State = {
   ids: [],
   q: 0
 };
 
-/*
- * DISCLAIMER :
- * exemple d'une copie superficielle alors que l'on aurait besoin d'une copie profonde
- * voir commit suivant pour la solution
- */
 export const selectedItemsReducer = createReducer(
   initialState,
-  on(itemSelected, (state, {id}) => {
-    state.ids.push(id);
-    return {...state};
-  })
+  on(itemSelected, (state, {id}) => ({...state, ...{ids: [...state.ids, id]}}))
 );
+
+/* Ou :
+ {
+   const ids = [...state.ids, id];
+   return {...state, ids};
+ }
+*/
+

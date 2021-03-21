feffa0b5363d224d7af8bd8af8d4ddb7c2f32566
ElisePatrikainen <elise.patrikainen@gmail.com>
2021-03-21 15:49:46 +0100

    03_06

--- src/app/state/selected-items/selected-items.reducer.ts
+++ src/app/state/selected-items/selected-items.reducer.ts
@@ -1,9 +1,14 @@
 import { createReducer, on } from '@ngrx/store';
 import { itemSelected } from './selected-items.actions';
 
-const initialState = [];
+// interface !
+
+const initialState = {
+  id: 0,
+  q: 0
+};
 
 export const selectedItemsReducer = createReducer(
-    initialState,
-    on(itemSelected, (state, {id}) => state.concat(id))
+  initialState,
+  on(itemSelected, (state, {id}) => ({...state, id}))
 );

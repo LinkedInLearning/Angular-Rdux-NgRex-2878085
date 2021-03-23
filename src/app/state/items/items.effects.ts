b842a5dbe054ce68f064ff7fb78eae8eaaf98820
ElisePatrikainen <elise.patrikainen@gmail.com>
2021-03-23 08:14:57 +0100

    04_06

--- src/app/state/items/items.effects.ts
+++ src/app/state/items/items.effects.ts
@@ -1,6 +1,7 @@
-import { LoadItems } from './items.actions';
+import { LoadItems, itemsLoaded } from './items.actions';
 import { Injectable } from '@angular/core';
 import { createEffect, Actions, ofType } from '@ngrx/effects';
+import { mergeMap, map } from 'rxjs/operators';
 import { ItemsService } from 'src/app/services/items.service';
 
 @Injectable()
@@ -8,12 +9,19 @@ export class ItemsEffects {
 
     loadItems$ = createEffect(
         () => this.actions$.pipe(
-            ofType(LoadItems)
+            ofType(LoadItems),
+            mergeMap(
+                () => this.itemsService.getItems()
+                    .pipe(
+                        map(items => itemsLoaded({items}))
+                    )
+            )
         )
     );
 
     constructor(
-        private actions$: Actions
+        private actions$: Actions,
+        private itemsService: ItemsService
     ) {}
 
 }
